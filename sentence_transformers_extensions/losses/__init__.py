112623f930c50bf08dd8b90db6c78d5fa5c930e9
t-mesq <tiago.mesquita.98@tecnico.ulisboa.pt>
2021-06-09 20:52:41 +0100

    Disabling torchsort imports until a fix is found

--- sentence_transformers_extensions/losses/__init__.py
+++ sentence_transformers_extensions/losses/__init__.py
@@ -1,7 +1,7 @@
 from .ModularLosses import *
 from .MultiplePositivesAndNegativesRankingLoss import *
-from .MeanAveragePrecisionLoss import *
-from .NormalizedDiscountedCumulativeGainLoss import *
-from .NLLAndMAPLoss import *
-from .NLLAndNDCGLoss import *
+# from .MeanAveragePrecisionLoss import *
+# from .NormalizedDiscountedCumulativeGainLoss import *
+# from .NLLAndMAPLoss import *
+# from .NLLAndNDCGLoss import *
 from .TransposedMultiplePositivesAndNegativesRankingLoss import *
