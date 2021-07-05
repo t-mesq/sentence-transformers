0f01e7f13ffc844a7a6777a5a921d5c6e5fa4962
t-mesq <tiago.mesquita.98@tecnico.ulisboa.pt>
2021-07-05 19:54:11 +0100

    Update __init__.py

--- sentence_transformers_extensions/datasets/__init__.py
+++ sentence_transformers_extensions/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .RoundRobinConservativeRankingDataset import *
 from .InformationRetrievalTemperatureDataset import *
+from .InformationRetrievalWithTemplatesTemperatureDataset import *
 from .InformationRetrievalDataset import *
 from .RoundRobinRankingDataset import *
 from .weigthers import *
